ce0c40937c45c3d2088b45bfba9e72bb09065c76
James Britton <james.britton@rocksolidknowledge.com>
2024-10-15 16:15:25 +0100

    feat: removed ':adminui' from URN for application and scope and claims property

--- Rsk.OpenIddict.Utils/Constants/AdminUiConstants.cs
+++ Rsk.OpenIddict.Utils/Constants/AdminUiConstants.cs
@@ -2,10 +2,10 @@
 
 public static class AdminUiConstants
 {
-    public const string ApplicationPropertyBagUrnPrefix = "urn:com:rocksolidknowledge:adminui:application:property:";
+    public const string ApplicationPropertyBagUrnPrefix = "urn:com:rocksolidknowledge:application:property:";
     public const string ApplicationPropertyClaims = $"{ApplicationPropertyBagUrnPrefix}claims";
     public const string ApplicationAdminUiClientType = $"{ApplicationPropertyBagUrnPrefix}type";
 
-    public const string ScopePropertyBagUrnPrefix = "urn:com:rocksolidknowledge:adminui:scope:property:";
+    public const string ScopePropertyBagUrnPrefix = "urn:com:rocksolidknowledge:scope:property:";
     public const string ScopePropertyClaims = $"{ScopePropertyBagUrnPrefix}claims";
 }
